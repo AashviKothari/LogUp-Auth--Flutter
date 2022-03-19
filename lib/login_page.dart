6d1dc5650326470c435e32c56b3e873510573038
Aashvi Kothari <aashvikothari78@gmail.com>
2022-03-19 12:43:25 +0530

    Third Commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -10,8 +12,68 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+
+double w = MediaQuery.of(context).size.width;
+double h = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+
+            width: w,
+            height: h*0.3,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "img/loginimg.png"
+                ),
+                fit: BoxFit.cover
+              )
+            ),
+
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20,right: 20),
+            width: w,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Hello!",
+                  style: TextStyle(
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Text(
+                  "Sign into your Account",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.grey[500]
+                  ),
+                ),
+                SizedBox(height: 40),
+                TextField(
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.0,
+                        
+                      )  
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30)
+                    )
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
